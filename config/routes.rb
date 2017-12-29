1d9ca773a8fd69b929c12c62f58269ef05021d54
allan-fk <allanfk@protonmail.com>
2017-12-29 12:27:15 +0100

    Création controller reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :path=>'',
-                     :path_names=> {:sign_in=> 'login',:sign_out=> 'logout', :edit=> 'profile'},
-                     :controllers=> {:registrations=> 'registrations'}
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile' },
+                     controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   resources :users, only: [:show]
-  resources :rooms
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
   resources :photos
 end
---
+++ spec/factories/reservations.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :reservation do
+    user nil
+    room nil
+    start_date "2017-12-27 23:53:35"
+    end_date "2017-12-27 23:53:35"
+    price 1
+    total 1
+  end
+end
