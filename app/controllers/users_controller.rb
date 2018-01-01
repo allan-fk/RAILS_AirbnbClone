a495f6dc371f8f79a936ed9051c3455e40689bed
allan-fk <allanfk@protonmail.com>
2018-01-01 21:25:05 +0100

    Amélioration des reviews + affichage sur page user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @rooms = @user.rooms
+    # rooms = tout les annonces de l'utilisateurs
   end
 end
